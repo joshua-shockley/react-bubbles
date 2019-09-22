117b95e5c35e8d98b498c3fdb89b1120b6615772
Joshua-Shockley <thelumbashockley@gmail.com>
2019-09-21 21:26:57 -0500

    removed that pesky redux stuff that i missed earlier

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,15 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import {createStore, applyMiddleware, compose} from 'redux';
-import { Provider} from 'react-redux';
-import thunk from 'redux-thunk';
-import {reducer} from './reducer/index.js';
 import App from "./App";
 
 import {withRouter} from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-    <Provider store={store}>
         <Router>
             <App />
         </Router>
-    </Provider>
 , rootElement);
